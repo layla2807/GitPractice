48da04df38404cb22b42e54c821b5b688da5789f
Phuong <phuongtran28072008@gmail.com>
2025-07-23 11:13:59 -0700

    change1

--- GitPractice/ContentView.swift
+++ GitPractice/ContentView.swift
@@ -14,6 +14,7 @@ struct ContentView: View {
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("My favorite color is blue.")
+            Text("Change1")
         }
         .padding()
     }
